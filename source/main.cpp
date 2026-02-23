4bfb8de4bb9da893a0b82571315146574b61ca11
alex_stavrin <alekosstavrinoudakis@gmail.com>
2026-02-24 00:53:21 +0200

    making shader program

--- source/main.cpp
+++ source/main.cpp
@@ -51,6 +51,72 @@ int main()
     // Set Clear Color
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+    float triangle_vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+    };
+
+    unsigned vertex_buffer_object;
+    glGenBuffers(1, &vertex_buffer_object);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
+
+    // Variables for checking compilation of shaders    
+    int success;
+    char info_log[512];
+    
+    // Vertex Shader
+    const char* vertex_shader_source = "#version 330 core\n"
+        "layout (location = 0) in vec3 vertex_position;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(vertex_position, 1.0);\n"
+        "}\0";
+    unsigned vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        ErrorPrinter::PrintError(info_log);
+    }
+        
+    // Fragment Shader
+    const char* fragment_shader_source = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
+    unsigned fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        ErrorPrinter::PrintError(info_log);
+    }
+
+    // Shader program
+    unsigned shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+        ErrorPrinter::PrintError(info_log);
+    }
+    glUseProgram(shader_program);
+
+    // Delete shaders after linking
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
     // Game loop
     while(!glfwWindowShouldClose(window))
     {
