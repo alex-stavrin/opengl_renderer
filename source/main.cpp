f3dc44863c083f0d5e5ef71697bdd434bb47a20d
alex_stavrin <alekosstavrinoudakis@gmail.com>
2026-02-23 22:03:28 +0200

    fixed window resize and color buffer. added close on escape key (input)

--- source/main.cpp
+++ source/main.cpp
@@ -9,6 +9,14 @@ void on_window_size_changed(GLFWwindow* window, int new_width, int new_height)
     glViewport(0, 0, new_width, new_height);
 }
 
+void process_input(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 int main()
 {
     glfwInit();
@@ -40,9 +48,18 @@ int main()
     // On window size changed. Change also the viewport
     glfwSetFramebufferSizeCallback(window, on_window_size_changed);
 
-    // Render loop
+    // Set Clear Color
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    // Game loop
     while(!glfwWindowShouldClose(window))
     {
+        // Input
+        process_input(window);
+
+        // Render
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
