6b6a1a23ed55773867729a45d641774323a2000d
alex-stavrin <alekosstavrinoudakis@gmail.com>
2026-02-24 10:39:20 +0200

    vertex attribute

--- source/main.cpp
+++ source/main.cpp
@@ -57,10 +57,15 @@ int main()
         0.0f, 0.5f, 0.0f,
     };
 
+    // Make the buffer
     unsigned vertex_buffer_object;
     glGenBuffers(1, &vertex_buffer_object);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
+
+    // Tell the vertex shader how to interpret the buffer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     // Variables for checking compilation of shaders    
     int success;
