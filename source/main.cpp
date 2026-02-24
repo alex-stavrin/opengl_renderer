48f43edb60a2c164b736ba276ff82c54c26f13aa
alex-stavrin <alekosstavrinoudakis@gmail.com>
2026-02-24 14:45:22 +0200

    drawing triangle

--- source/main.cpp
+++ source/main.cpp
@@ -64,6 +64,9 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
 
     // Tell the vertex shader how to interpret the buffer
+    unsigned vertex_array_object;
+    glGenVertexArrays(1, &vertex_array_object);
+    glBindVertexArray(vertex_array_object);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -130,6 +133,7 @@ int main()
 
         // Render
         glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
