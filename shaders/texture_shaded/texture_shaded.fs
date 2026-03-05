bbe22456336c1b2eff47cc74591b8561a2f5f61e
alex-stavrin <alekosstavrinoudakis@gmail.com>
2026-03-05 18:55:47 +0200

    added diffuse and specular maps

--- shaders/texture_shaded/texture_shaded.fs
+++ shaders/texture_shaded/texture_shaded.fs
@@ -2,9 +2,8 @@
 
 struct Material
 {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
     float shininess;
 };
 
@@ -23,9 +22,6 @@ in vec2 texture_coordinates;
 in vec3 normal;
 in vec3 fragment_position;
 
-uniform sampler2D texture0;
-uniform sampler2D texture1;
-
 uniform vec3 camera_position;
 
 uniform Material material;
@@ -34,26 +30,27 @@ uniform Light light;
 
 void main()
 {
-    vec4 texture = mix(texture(texture0, texture_coordinates), texture(texture1, texture_coordinates), 0.2);
-    vec3 object_color = texture.rgb;
+    vec4 diffuse_texture = texture(material.diffuse, texture_coordinates);
+    vec3 object_color = diffuse_texture.rgb;
 
     // ambient
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light.ambient * object_color;
 
     // diffuse
     vec3 actual_normal = normalize(normal);
     vec3 light_direction = normalize(light.position - fragment_position);
     float diffuse_amount = max(dot(actual_normal, light_direction), 0.0);
-    vec3 diffuse = (diffuse_amount * material.diffuse) * light.diffuse;
+    vec3 diffuse = diffuse_amount * object_color * light.diffuse;
 
     // specular
     vec3 camera_direction = normalize(camera_position - fragment_position);
     vec3 reflection_direction = reflect(-light_direction, actual_normal);
     float specular_amount = pow(max(dot(camera_direction, reflection_direction),
         0.0), material.shininess);
-    vec3 specular =  (specular_amount * material.specular) * light.specular;
+    vec3 specular_texture = vec3(texture(material.specular, texture_coordinates));
+    vec3 specular =  (specular_amount * specular_texture) * light.specular;
 
     // final
-    vec3 final_color = object_color * (ambient + diffuse + specular);
+    vec3 final_color = ambient + diffuse + specular;
     FragColor = vec4(final_color, 1.0);
 }
--- source/main.cpp
+++ source/main.cpp
@@ -90,11 +90,11 @@ int main()
     glEnableVertexAttribArray(2);
 
     stbi_set_flip_vertically_on_load(true);  
-    // Load image
+    // Load diffuse image
     int width;
     int height;
     int number_of_color_channels;
-    unsigned char* data = stbi_load("./assets/container.jpg", &width, &height, &number_of_color_channels, 0);
+    unsigned char* data = stbi_load("./assets/container_diffuse.png", &width, &height, &number_of_color_channels, 0);
     if(data)
     {
         unsigned texture;
@@ -113,7 +113,7 @@ int main()
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     
         // pass image to texture
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     
         // generate mip maps
         glGenerateMipmap(GL_TEXTURE_2D);    
@@ -124,14 +124,14 @@ int main()
     }
     stbi_image_free(data);
 
-    data = stbi_load("./assets/awesomeface.png", &width, &height, &number_of_color_channels, 0);
+    data = stbi_load("./assets/container_specular.png", &width, &height, &number_of_color_channels, 0);
     if(data)
     {
         unsigned texture;
         glGenTextures(1, &texture);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture);
-
+    
         // texture wrapping
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -141,12 +141,12 @@ int main()
     
         // minmap filtering (for minification. texture < space)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        
+    
         // pass image to texture
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     
         // generate mip maps
-        glGenerateMipmap(GL_TEXTURE_2D);   
+        glGenerateMipmap(GL_TEXTURE_2D);    
     }
     else
     {
@@ -173,33 +173,29 @@ int main()
 
     light_shader.SetVector3("light_color", light_color);
 
-    Shader crate_shader("./shaders/crate/crate.vs", "./shaders/crate/crate.fs");
-    OpWindow::RegisterShader(&crate_shader);
-    crate_shader.Use();
+    Shader texture_shaded("./shaders/texture_shaded/texture_shaded.vs", "./shaders/texture_shaded/texture_shaded.fs");
+    OpWindow::RegisterShader(&texture_shaded);
+    texture_shaded.Use();
 
-    crate_shader.SetVector3("light.position", light_position);
-    crate_shader.SetVector3("light.ambient", glm::vec3(0.2f));
-    crate_shader.SetVector3("light.diffuse", glm::vec3(0.5f));
-    crate_shader.SetVector3("light.specular", glm::vec3(1.0f));
+    texture_shaded.SetVector3("light.position", light_position);
+    texture_shaded.SetVector3("light.ambient", glm::vec3(0.2f));
+    texture_shaded.SetVector3("light.diffuse", glm::vec3(0.8f));
+    texture_shaded.SetVector3("light.specular", glm::vec3(1.0f));
    
-    crate_shader.SetVector3("material.ambient", glm::vec3(1.0f));
-    crate_shader.SetVector3("material.diffuse", glm::vec3(1.0f));
-    crate_shader.SetVector3("material.specular", glm::vec3(1.0f));
-    crate_shader.SetFloat("material.shininess", 32.0f);
-
-    crate_shader.SetInt("texture0", 0);
-    crate_shader.SetInt("texture1", 1);
+    texture_shaded.SetInt("material.diffuse", 0);
+    texture_shaded.SetInt("material.specular", 1);
+    texture_shaded.SetFloat("material.shininess", 32.0f);
 
     // model matrix
     glm::mat4 crate_model_matrix = glm::mat4(1.0f);
     crate_model_matrix = glm::translate(crate_model_matrix, glm::vec3(0.0,0.5,2.0));
     
-    crate_shader.SetMatrix("model_matrix", crate_model_matrix);
-    crate_shader.SetMatrix("projection_matrix", projection_matrix);
+    texture_shaded.SetMatrix("model_matrix", crate_model_matrix);
+    texture_shaded.SetMatrix("projection_matrix", projection_matrix);
 
     glEnable(GL_DEPTH_TEST);
     
-    float last_frame_time = 1;
+    float last_frame_time = static_cast<float>(glfwGetTime());
 
     // Game loop
     while(!glfwWindowShouldClose(OpWindow::GetWindowGLFW()))
@@ -217,9 +213,9 @@ int main()
         // Render
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        crate_shader.Use();
-        crate_shader.SetMatrix("view_matrix", camera.GetViewMatrix());
-        crate_shader.SetVector3("camera_position", camera.GetPosition());
+        texture_shaded.Use();
+        texture_shaded.SetMatrix("view_matrix", camera.GetViewMatrix());
+        texture_shaded.SetVector3("camera_position", camera.GetPosition());
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         light_shader.Use();
